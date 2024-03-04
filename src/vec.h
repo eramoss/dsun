1d4ce8e942afbc92b6dc4747d806eb409a10c21d
eramoss <edulramos@outlook.com>
2024-03-04 16:07:33 -0300

    basic functions to vec

--- src/vec.h
+++ src/vec.h
@@ -5,17 +5,22 @@
 #define DSUN_VEC_H
 
 #include <memory>
-
+#include <optional>
 template <class T>
 class Vec {
 private:
-    std::unique_ptr<T[]> data;
+    std::unique_ptr<T[]> ptr;
     uint32_t length;
     uint32_t cap;
     static constexpr uint32_t INITIAL_SIZE = 16;
 public:
     Vec();
     void from_slice(T* slice, uint32_t len);
+    std::optional<T> get(uint32_t index);
+    std::optional<T*> get_mut(uint32_t index);
+    void push(T value);
+
+
     [[nodiscard]] uint32_t len() const {
         return length;
     }
@@ -27,16 +32,40 @@ public:
 template <class T>
 Vec<T>::Vec() : length(0), cap(INITIAL_SIZE) {
     static_assert(std::size_t{ sizeof(T) } > 0, "T must be complete type");
-    data = std::make_unique<T[]>(cap);
+    ptr = std::make_unique<T[]>(cap);
 }
 
 template <class T>
 void Vec<T>::from_slice(T* slice, uint32_t len) {
     length = len;
     cap = len * 2;
-    data = std::make_unique<T[]>(cap);
-    std::copy(slice, slice + length, data.get());
+    ptr = std::make_unique<T[]>(cap);
+    std::copy(slice, slice + length, ptr.get());
 }
+
+template <class T>
+std::optional<T> Vec<T>::get(uint32_t index) {
+    return this->ptr.get()[index] ? std::optional<T>(this->ptr.get()[index]) : std::nullopt;
+}
+
+template <class T>
+std::optional<T*> Vec<T>::get_mut(uint32_t index) {
+    return this->ptr.get()[index] ? std::optional<T*>(&this->ptr.get()[index]) : std::nullopt;
+}
+
+template <class T>
+void Vec<T>::push(T value) {
+    if (length == cap) {
+        cap *= 2;
+        auto new_ptr = std::make_unique<T[]>(cap);
+        std::copy(ptr.get(), ptr.get() + length, new_ptr.get());
+        ptr = std::move(new_ptr);
+    }
+    ptr.get()[length++] = value;
+}
+
+
+
 
 
 #endif //DSUN_VEC_H
--- tests/test_vec.cpp
+++ tests/test_vec.cpp
@@ -19,6 +19,30 @@ TEST_F(VecTest, FromSliceTest) {
   EXPECT_EQ(vec.capacity(), 10);
 }
 
+TEST_F(VecTest, GetVec) {
+  int arr[5] = { 1, 2, 3, 4, 5 };
+  vec.from_slice(arr, 5);
+
+  EXPECT_EQ(vec.get(0).value(), 1);
+}
+
+TEST_F(VecTest, GetMutVec) {
+  int arr[5] = { 1, 2, 3, 4, 5 };
+  vec.from_slice(arr, 5);
+
+  int* ptr = vec.get_mut(0).value();
+  *ptr = 10;
+  EXPECT_EQ(vec.get(0).value(), 10);
+}
+
+TEST_F(VecTest, PushVec) {
+  int arr[5] = { 1, 2, 3, 4, 5 };
+  vec.from_slice(arr, 5);
+  vec.push(6);
+
+  EXPECT_EQ(vec.get(5).value(), 6);
+}
+
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
