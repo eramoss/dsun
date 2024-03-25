0e2ee081e907211f55503922d04eccbf89681bd6
eramoss <edulramos@outlook.com>
2024-03-25 10:45:16 -0300

    adjust script test to better output

--- scripts/test.py
+++ scripts/test.py
@@ -1,25 +1,31 @@
 import os
 import subprocess
 import sys
+from build import build_cmake_project
 
 def run_ctest(build_dir):
-    """
+    '''
     Run tests with CTest.
 
     Args:
         build_dir (str): Path to the build directory of the CMake project.
-    """
+    '''
+
     os.chdir(build_dir)
-
-    ctest_command = ['ctest', "-V"]
-    subprocess.run(ctest_command, check=True)
-
-if __name__ == "__main__":
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-
-    build_directory = os.path.join(script_dir, "../build")
-    if not os.path.exists(build_directory):
-        print("Build directory not found. Run the CMake build script first.")
+    ctest_command = ['ctest', '-V' ,'-C' , 'Debug', '--output-on-failure']
+    try:
+        subprocess.run(ctest_command, check=True)
+    except subprocess.CalledProcessError:
         sys.exit(1)
 
-    run_ctest(build_directory)
+if __name__ == '__main__':
+    self_dir = os.path.dirname(os.path.abspath(__file__))
+    source_dir = os.path.join(self_dir, '..')
+    build_dir =  os.path.join(self_dir,'../build')
+    build_cmake_project(source_dir, build_dir, 'Debug')
+
+    if not os.path.exists(build_dir):
+        print('Build directory not found. Run the CMake build script first.')
+        sys.exit(1)
+
+    run_ctest(build_dir)
