274c8cf188fafa678367657458131ca4604dd49e
eramoss <edulramos@outlook.com>
2024-04-26 10:12:03 -0300

    constructors

--- src/bst.h
+++ src/bst.h
@@ -17,6 +17,33 @@ namespace dsun {
     };
     using node_ptr_opt = std::optional<std::unique_ptr<Node>>;
     node_ptr_opt root;
+
+  public:
+    /*Constructors*/
+    BST() : root(std::nullopt) {}
+    BST(const BST<T>& other) {
+      root = clone(other.root);
+    }
+    BST<T>& operator=(const BST<T>& other) {
+      root = clone(other.root);
+      return *this;
+    }
+    static BST<T> from_slice(const T* data, size_t len) {
+      BST<T> bst;
+      for (size_t i = 0; i < len; i++) {
+        bst.insert(data[i]);
+      }
+      return bst;
+    }
+    static BST<T> from_list(const std::initializer_list<T> list) {
+      BST<T> bst;
+      for (const auto& item : list) {
+        bst.insert(item);
+      }
+      return bst;
+    }
+
+
   };
 }
 
